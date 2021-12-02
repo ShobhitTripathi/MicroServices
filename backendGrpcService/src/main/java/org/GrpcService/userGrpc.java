a0f12761e108e12d9a4e5b20b85aae043a106379
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-12-02 22:34:43 +0530

    Add files via upload

---
+++ backendGrpcService/src/main/java/org/GrpcService/userGrpc.java
@@ -0,0 +1,423 @@
+package org.benz.GrpcService;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.15.0)",
+    comments = "Source: user.proto")
+public final class userGrpc {
+
+  private userGrpc() {}
+
+  public static final String SERVICE_NAME = "user";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<org.benz.GrpcService.User.Empty,
+      org.benz.GrpcService.User.APIResponse> getGetAllUsersMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "getAllUsers",
+      requestType = org.benz.GrpcService.User.Empty.class,
+      responseType = org.benz.GrpcService.User.APIResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.SERVER_STREAMING)
+  public static io.grpc.MethodDescriptor<org.benz.GrpcService.User.Empty,
+      org.benz.GrpcService.User.APIResponse> getGetAllUsersMethod() {
+    io.grpc.MethodDescriptor<org.benz.GrpcService.User.Empty, org.benz.GrpcService.User.APIResponse> getGetAllUsersMethod;
+    if ((getGetAllUsersMethod = userGrpc.getGetAllUsersMethod) == null) {
+      synchronized (userGrpc.class) {
+        if ((getGetAllUsersMethod = userGrpc.getGetAllUsersMethod) == null) {
+          userGrpc.getGetAllUsersMethod = getGetAllUsersMethod = 
+              io.grpc.MethodDescriptor.<org.benz.GrpcService.User.Empty, org.benz.GrpcService.User.APIResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.SERVER_STREAMING)
+              .setFullMethodName(generateFullMethodName(
+                  "user", "getAllUsers"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  org.benz.GrpcService.User.Empty.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  org.benz.GrpcService.User.APIResponse.getDefaultInstance()))
+                  .setSchemaDescriptor(new userMethodDescriptorSupplier("getAllUsers"))
+                  .build();
+          }
+        }
+     }
+     return getGetAllUsersMethod;
+  }
+
+  private static volatile io.grpc.MethodDescriptor<org.benz.GrpcService.User.UserRequest,
+      org.benz.GrpcService.User.APIResponse> getAddUserMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "addUser",
+      requestType = org.benz.GrpcService.User.UserRequest.class,
+      responseType = org.benz.GrpcService.User.APIResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<org.benz.GrpcService.User.UserRequest,
+      org.benz.GrpcService.User.APIResponse> getAddUserMethod() {
+    io.grpc.MethodDescriptor<org.benz.GrpcService.User.UserRequest, org.benz.GrpcService.User.APIResponse> getAddUserMethod;
+    if ((getAddUserMethod = userGrpc.getAddUserMethod) == null) {
+      synchronized (userGrpc.class) {
+        if ((getAddUserMethod = userGrpc.getAddUserMethod) == null) {
+          userGrpc.getAddUserMethod = getAddUserMethod = 
+              io.grpc.MethodDescriptor.<org.benz.GrpcService.User.UserRequest, org.benz.GrpcService.User.APIResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(
+                  "user", "addUser"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  org.benz.GrpcService.User.UserRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  org.benz.GrpcService.User.APIResponse.getDefaultInstance()))
+                  .setSchemaDescriptor(new userMethodDescriptorSupplier("addUser"))
+                  .build();
+          }
+        }
+     }
+     return getAddUserMethod;
+  }
+
+  private static volatile io.grpc.MethodDescriptor<org.benz.GrpcService.User.UserRequest,
+      org.benz.GrpcService.User.APIResponse> getUpdateUserMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "updateUser",
+      requestType = org.benz.GrpcService.User.UserRequest.class,
+      responseType = org.benz.GrpcService.User.APIResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<org.benz.GrpcService.User.UserRequest,
+      org.benz.GrpcService.User.APIResponse> getUpdateUserMethod() {
+    io.grpc.MethodDescriptor<org.benz.GrpcService.User.UserRequest, org.benz.GrpcService.User.APIResponse> getUpdateUserMethod;
+    if ((getUpdateUserMethod = userGrpc.getUpdateUserMethod) == null) {
+      synchronized (userGrpc.class) {
+        if ((getUpdateUserMethod = userGrpc.getUpdateUserMethod) == null) {
+          userGrpc.getUpdateUserMethod = getUpdateUserMethod = 
+              io.grpc.MethodDescriptor.<org.benz.GrpcService.User.UserRequest, org.benz.GrpcService.User.APIResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(
+                  "user", "updateUser"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  org.benz.GrpcService.User.UserRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  org.benz.GrpcService.User.APIResponse.getDefaultInstance()))
+                  .setSchemaDescriptor(new userMethodDescriptorSupplier("updateUser"))
+                  .build();
+          }
+        }
+     }
+     return getUpdateUserMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static userStub newStub(io.grpc.Channel channel) {
+    return new userStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static userBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new userBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static userFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new userFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class userImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void getAllUsers(org.benz.GrpcService.User.Empty request,
+        io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(getGetAllUsersMethod(), responseObserver);
+    }
+
+    /**
+     */
+    public void addUser(org.benz.GrpcService.User.UserRequest request,
+        io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(getAddUserMethod(), responseObserver);
+    }
+
+    /**
+     */
+    public void updateUser(org.benz.GrpcService.User.UserRequest request,
+        io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(getUpdateUserMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getGetAllUsersMethod(),
+            asyncServerStreamingCall(
+              new MethodHandlers<
+                org.benz.GrpcService.User.Empty,
+                org.benz.GrpcService.User.APIResponse>(
+                  this, METHODID_GET_ALL_USERS)))
+          .addMethod(
+            getAddUserMethod(),
+            asyncUnaryCall(
+              new MethodHandlers<
+                org.benz.GrpcService.User.UserRequest,
+                org.benz.GrpcService.User.APIResponse>(
+                  this, METHODID_ADD_USER)))
+          .addMethod(
+            getUpdateUserMethod(),
+            asyncUnaryCall(
+              new MethodHandlers<
+                org.benz.GrpcService.User.UserRequest,
+                org.benz.GrpcService.User.APIResponse>(
+                  this, METHODID_UPDATE_USER)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class userStub extends io.grpc.stub.AbstractStub<userStub> {
+    private userStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private userStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected userStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new userStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void getAllUsers(org.benz.GrpcService.User.Empty request,
+        io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse> responseObserver) {
+      asyncServerStreamingCall(
+          getChannel().newCall(getGetAllUsersMethod(), getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void addUser(org.benz.GrpcService.User.UserRequest request,
+        io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(getAddUserMethod(), getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void updateUser(org.benz.GrpcService.User.UserRequest request,
+        io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(getUpdateUserMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class userBlockingStub extends io.grpc.stub.AbstractStub<userBlockingStub> {
+    private userBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private userBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected userBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new userBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public java.util.Iterator<org.benz.GrpcService.User.APIResponse> getAllUsers(
+        org.benz.GrpcService.User.Empty request) {
+      return blockingServerStreamingCall(
+          getChannel(), getGetAllUsersMethod(), getCallOptions(), request);
+    }
+
+    /**
+     */
+    public org.benz.GrpcService.User.APIResponse addUser(org.benz.GrpcService.User.UserRequest request) {
+      return blockingUnaryCall(
+          getChannel(), getAddUserMethod(), getCallOptions(), request);
+    }
+
+    /**
+     */
+    public org.benz.GrpcService.User.APIResponse updateUser(org.benz.GrpcService.User.UserRequest request) {
+      return blockingUnaryCall(
+          getChannel(), getUpdateUserMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class userFutureStub extends io.grpc.stub.AbstractStub<userFutureStub> {
+    private userFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private userFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected userFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new userFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<org.benz.GrpcService.User.APIResponse> addUser(
+        org.benz.GrpcService.User.UserRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(getAddUserMethod(), getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<org.benz.GrpcService.User.APIResponse> updateUser(
+        org.benz.GrpcService.User.UserRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(getUpdateUserMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_GET_ALL_USERS = 0;
+  private static final int METHODID_ADD_USER = 1;
+  private static final int METHODID_UPDATE_USER = 2;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final userImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(userImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_GET_ALL_USERS:
+          serviceImpl.getAllUsers((org.benz.GrpcService.User.Empty) request,
+              (io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse>) responseObserver);
+          break;
+        case METHODID_ADD_USER:
+          serviceImpl.addUser((org.benz.GrpcService.User.UserRequest) request,
+              (io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse>) responseObserver);
+          break;
+        case METHODID_UPDATE_USER:
+          serviceImpl.updateUser((org.benz.GrpcService.User.UserRequest) request,
+              (io.grpc.stub.StreamObserver<org.benz.GrpcService.User.APIResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class userBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    userBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return org.benz.GrpcService.User.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("user");
+    }
+  }
+
+  private static final class userFileDescriptorSupplier
+      extends userBaseDescriptorSupplier {
+    userFileDescriptorSupplier() {}
+  }
+
+  private static final class userMethodDescriptorSupplier
+      extends userBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    userMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (userGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new userFileDescriptorSupplier())
+              .addMethod(getGetAllUsersMethod())
+              .addMethod(getAddUserMethod())
+              .addMethod(getUpdateUserMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
